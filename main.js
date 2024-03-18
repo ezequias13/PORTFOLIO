ffe0edb235c77109c1e2cf5b07c5584d4ac71172
ezequias13 <ezequiassouaa@gmail.com>
2024-03-18 20:43:22 -0300

    arquivos do portfolio

---
+++ main.js
@@ -0,0 +1,8 @@
+/*let Click = document.getElementById("contact");
+
+Click.onclick = function () {
+    let nome = prompt("Qual Seu Nome?");
+    let contact = prompt("Qual seu Telefone para contao?");
+    alert("Olá " + nome + ", Entrarei Em Contato Com esse numero!! " + contact);
+};*/
+
